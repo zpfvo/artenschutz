809bb04e1ebde3e4f3b7085c985f570c5649ba81
zpfvo <florian.voit@gmail.com>
2020-07-07 23:06:42 +0200

    [wip]

--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart' show BuildContext, Widget, StatelessWidget,
-  BottomNavigationBar, BottomNavigationBarItem,
-  Text, Icon, Icons, Colors; 
+import 'package:flutter/material.dart'
+    show
+        BuildContext,
+        Widget,
+        StatelessWidget,
+        BottomNavigationBar,
+        BottomNavigationBarItem,
+        Text,
+        Icon,
+        Icons,
+        Colors;
 import 'package:meta/meta.dart' show required;
 import 'package:redux/redux.dart' show Store;
-import 'package:font_awesome_flutter/font_awesome_flutter.dart' show FontAwesomeIcons;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'
+    show FontAwesomeIcons;
 import 'package:flutter_redux/flutter_redux.dart' show StoreConnector;
 import 'package:artenschatuz_am_gebaeude/models/models.dart' show AppState;
-import 'package:artenschatuz_am_gebaeude/actions/actions.dart' show SelectNavigationBarIndex;
+import 'package:artenschatuz_am_gebaeude/actions/actions.dart'
+    show SelectNavigationBarIndex;
 
 class AppNavBar extends StatelessWidget {
   @override
@@ -31,26 +41,28 @@ class AppNavBar extends StatelessWidget {
               ),
             ],
             currentIndex: vm.bottomNavigationBarSelectedIndex,
-            selectedItemColor: Colors.amber[800],
-            onTap: (index) => vm.onTap(index)
-          );
+            selectedItemColor: Colors.black,
+            onTap: (index) => vm.onTap(index));
       },
     );
   }
 }
 
-
 class _ViewModel {
   final int bottomNavigationBarSelectedIndex;
   final Function(int) onTap;
 
-  _ViewModel({@required this.bottomNavigationBarSelectedIndex, @required this.onTap});
+  _ViewModel(
+      {@required this.bottomNavigationBarSelectedIndex, @required this.onTap});
 
   factory _ViewModel.create(Store<AppState> store) {
     _onTap(index) {
       store.dispatch(SelectNavigationBarIndex(index));
     }
 
-    return _ViewModel(bottomNavigationBarSelectedIndex: store.state.bottomNavigationBarSelectedIndex, onTap: _onTap);
+    return _ViewModel(
+        bottomNavigationBarSelectedIndex:
+            store.state.bottomNavigationBarSelectedIndex,
+        onTap: _onTap);
   }
 }
